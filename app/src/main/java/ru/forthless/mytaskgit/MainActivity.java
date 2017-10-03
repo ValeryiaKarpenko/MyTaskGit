01ed2bdbf64516bf53cb17c37b4b96ad44ef6918
Valeryia Karpenko <valeria.dmitrenko97@mail.ru>
2017-10-03 19:48:41 +0300

    add more some changes
    (cherry picked from commit ce89414831955e7c59a98206a5fa4849b32993e3)
    
    # Conflicts:
    #	app/src/main/java/ru/forthless/mytaskgit/MainActivity.java

--- app/src/main/java/ru/forthless/mytaskgit/MainActivity.java
+++ app/src/main/java/ru/forthless/mytaskgit/MainActivity.java
@@ -9,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // another changes
-        // anoter changes2
+        //some changes
     }
 }
